349615135e8e8958688314b32e523a9abbc71e1c
Atte Huhtakangas <atte.huhtakangas@gmail.com>
2017-08-26 22:27:15 +0300

    Setup some initial stuff

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,16 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import { injectGlobal } from 'styled-components';
+
+// tslint:disable-next-line:no-unused-expression
+injectGlobal`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+  }
+`;
 
 ReactDOM.render(
   <App />,
